3c592faacba4bc74d9b3ef1d0f419bc2cc39f047
Joe Hoyle <joehoyle@gmail.com>
2020-12-03 00:58:21 +0000

    update

--- npm-module/bin/index.js
+++ npm-module/bin/index.js
@@ -2,17 +2,38 @@
 const rollup = require('rollup');
 //const config = require('../rollup.config.js');
 const typescript = require('@rollup/plugin-typescript');
+const commonjs = require('@rollup/plugin-commonjs');
+const nodeResolve = require('@rollup/plugin-node-resolve').nodeResolve;
 
 const projectDir = '.';
-
-rollup.rollup({
-    input: `${ __dirname }/../index.tsx`,
-    plugins: [
-		typescript({
-			// tsconfig: `${ projectDir }/tsconfig.json`,
-			jsx: 'react',
-			rootDir: `${ projectDir }/`,
-			tslib: require.resolve('tslib'),
-		})
-	]
-} )
+console.warn(require.resolve('tslib'));
+rollup
+	.rollup({
+		input: `${__dirname}/../theme/index.tsx`,
+		plugins: [
+			typescript({
+				// tsconfig: `${ projectDir }/tsconfig.json`,
+				jsx: 'react',
+				rootDir: `${projectDir}/`,
+				tslib: require('tslib'),
+				typescript: require('typescript'),
+				paths: {
+					tslib: [require.resolve('tslib')],
+				},
+			}),
+			commonjs(),
+			nodeResolve({
+				browser: true,
+			}),
+		],
+	})
+	.then((bundle) => {
+		bundle.write({
+			file: './build/csj/index.js',
+			format: 'cjs'
+		});
+		bundle.write({
+			file: './build/es/index.js',
+			format: 'es'
+		});
+	});
